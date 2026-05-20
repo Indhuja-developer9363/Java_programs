e1d1d811c67c5b7de811fca0c4baeec0e65017cd
Indhuja-developer9363 <gopalindhuja@gmail.com>
2026-05-20 10:38:11 +0530

    Encapsulation

---
+++ Encap.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Employee{
+    private int marks;
+    public void set(int marks){
+        this.marks=marks;
+    }
+    public int get(){
+        return marks;
+    }
+}
+public class Encap{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int marks = sc.nextInt();
+        Employee e = new Employee();
+        e.set(marks); 
+        System.out.println(e.get());
+    }
+}
