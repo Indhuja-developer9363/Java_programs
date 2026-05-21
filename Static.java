bfbe4154d0ca32ba7f05c71e25a615b5c8575ff8
Indhuja-developer9363 <gopalindhuja@gmail.com>
2026-05-21 14:10:18 +0530

    Static Variable

---
+++ Static.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Student{
+    static String college = "Shanmugha";  //static variable
+    int rollno;  //normal variable
+    Student(int rollno){
+        this.rollno = rollno;  //101
+    }
+    void display(){
+        System.out.println(college+" "+rollno);
+    }
+}
+public class Static{
+    public static void main(String[]args){
+        Student s = new Student(101);
+        Student st = new Student(102);
+        s.display();
+        st.display();
+    }
+}
