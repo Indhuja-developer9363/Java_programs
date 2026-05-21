475474ce85cc07d12f60a6e32f0dd42be221ffd8
Indhuja-developer9363 <gopalindhuja@gmail.com>
2026-05-21 14:44:18 +0530

    Static Block

---
+++ Staticblock.java
@@ -0,0 +1,8 @@
+public class Staticblock{
+    static{
+        System.out.println("Shanmmugha Engineering");
+    }
+    public static void main(String[]args){
+        System.out.println("College");
+    }
+}
