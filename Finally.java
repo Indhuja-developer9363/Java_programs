a70aadcad70f1bc08353a6ba4c1f0a42f10b0b13
Indhuja-developer9363 <gopalindhuja@gmail.com>
2026-05-25 11:07:35 +0530

    ArrayList

---
+++ Finally.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Finally{
+    public static void main(String[]args){
+        Scanner s = new Scanner(System.in);
+        try{
+            int num = s.nextInt();  //a
+            System.out.println(num);
+        }
+        catch(InputMismatchException e){
+            System.out.println("Input is not an numerical value");
+        }
+        finally{
+            System.out.println("Execution are completed");
+        }
+        System.out.println("Java");
+    }
+}
