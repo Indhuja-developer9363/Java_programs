5a108d415c8ae3a3a8e262df8147b8e8e2ec806a
Indhuja-developer9363 <gopalindhuja@gmail.com>
2026-05-30 09:47:41 +0530

    FileReader

---
+++ Filereaderh.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Filereaderh{
+    public static void main (String[]args){
+        try{
+            File file = new File("sample.txt");
+            Scanner reader = new Scanner(file);
+            while (reader.hasNextLine()){
+                String data = reader.nextLine();
+                System.out.println(data);
+            }
+            reader.close();
+        }
+        catch(Exception e){
+            System.out.println("Error Occured");
+        }
+    }
+}
